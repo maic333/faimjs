448308972aae88cffdf20b447d370c10681b416f
Maic <mihai.iliemihai@gmail.com>
2019-06-10 23:18:38 +0300

    Implement JWT authentication

--- src/core/faim-app-config.ts
+++ src/core/faim-app-config.ts
@@ -1,14 +1,22 @@
-interface FaimAppConfig {
+interface FaimAppConfig<UserModel> {
   // API endpoints prefix
   apiPrefix?: string;
   // path to controllers directory
   controllersPath?: string | null;
   // path to services directory
   servicesPath?: string | null;
+  // JWT authentication
+  authentication?: {
+    enable: boolean,
+    jwt: {
+      key: string
+    },
+    // tslint:disable-next-line no-any
+    onAuth: (loginData: any) => Promise<UserModel>,
+    checkAuth: (userData: UserModel) => Promise<boolean>
+  };
   // #TODO enable jsonschema validation?
   validation?: boolean;
-  // #TODO enable JWT authentication?
-  authentication?: boolean;
 }
 
 export default FaimAppConfig;
